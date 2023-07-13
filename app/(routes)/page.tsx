ad2f3cdaafac66df94f3bcdc2e977ba7ffe35d75
Matthew Fogarty <mattempyre@gmail.com>
2023-07-14 00:41:12 +0200

    Created Billboard Component and added it to home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,16 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
+import getBillboard from "@/actions/get-billboard";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("3894906e-1b7f-4892-8b36-59e07d4d7721");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 export default HomePage;
