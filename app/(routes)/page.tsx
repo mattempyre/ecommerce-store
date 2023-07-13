b6d2e7db18e6c4c145f36b580882fec4c603e0a1
Matthew Fogarty <mattempyre@gmail.com>
2023-07-14 00:54:38 +0200

    Added Featured Product Component and Added it to the home page.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,20 @@
 import Container from "@/components/ui/container";
 import Billboard from "@/components/ui/billboard";
+import ProductList from "@/components/product-list";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("3894906e-1b7f-4892-8b36-59e07d4d7721");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
