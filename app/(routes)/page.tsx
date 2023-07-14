640f6ee68c93bd3f2d2ae4c36778c8d88012af69
Matthew Fogarty <mattempyre@gmail.com>
2023-07-14 15:55:14 +0200

    Update Main page and container

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,10 @@
-import Container from "@/components/ui/container";
-import Billboard from "@/components/ui/billboard";
-import ProductList from "@/components/product-list";
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
@@ -12,11 +14,12 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
 };
+
 export default HomePage;
